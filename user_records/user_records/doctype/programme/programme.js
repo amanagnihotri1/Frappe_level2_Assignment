88aa09db39573893560fc67ebc92eb6dc33a8523
Aman Agnihotri <69078309+amanagnihotri1@users.noreply.github.com>
2024-05-06 17:14:42 +0530

    Update programme.js

--- user_records/user_records/doctype/programme/programme.js
+++ user_records/user_records/doctype/programme/programme.js
@@ -28,28 +28,41 @@ frappe.ui.form.on("Programme",{
            
         })
     },
-    preview_participant: function( cdt, cdn) {
-        var row = locals[cdt][cdn];
-        var student = row.participant;
-        frappe.call({
-            method: "show_student_picture",
-            args: {
-                "student": student
-            },
-            callback: function(r) {
-                if (r.message) {
-                    var dialog = new frappe.ui.Dialog({
-                        title: __('Participant Picture'),
-                        fields: [
-                            {
-                                fieldtype: 'HTML',
-                                options: r.message
-                            }
-                        ]
-                    });
-                    dialog.show();
-                }
+    frappe.ui.form.on('Participant',{
+    student_image:(frm,cdt,cdn)=>
+      {
+        let data=locals[cdt,cdn]
+         frappe.call({
+          method:'user_records.user_records.doctype.programme.programme.get_student_image',
+          args:{student_name:data.student_name},
+          callback:(res)=>{
+            if (response.message) {
+                const student_image = res.message;
+                // Call a function to show the preview with the student's profile image
+                show_student_image(student_image);
             }
+        }
         });
-    }
-});  
+      }   
+   })
+   function show_student_image(stu_image) {
+    // Create a modal dialog
+    var dialog = new frappe.ui.Dialog({
+        title: 'Image_dialogue',
+        fields: [
+            {
+                fieldtype: 'HTML',
+                fieldname: 'preview_box',
+                label: 'Image Preview',
+                options: `<img src="${stu_image}" style="max-width: 100%; max-height: 100%;" />`
+            }
+        ],
+        primary_action_label: 'Close',
+        primary_action: function() {
+            dialog.hide();
+        }
+    });
+
+    // Show the modal dialog
+    dialog.show();
+}
